119efc92d6cad623996674d8567b9ca46f797d67
NaT <treconweb@NaT.local>
2016-01-26 11:13:19 +0700

    Create pattern file no data in file

---
+++ insert-data.php
@@ -0,0 +1,1 @@
+insert-native.php
---
+++ sqlinjection.php
@@ -0,0 +1,8 @@
+ก่อนตัวแปร select
+$id = $_POST['id'];
+$id = mysql_real_escape_string($id);
+$sql = "select * member where id='$id'";
+
+
+1' or '1' = '1
+1'or'1'='1
